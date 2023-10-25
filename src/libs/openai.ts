f756cedd92fc6104238e8d8d96dc1e81251f3090
Rodrigo Munoz <rm117579@gmail.com>
2023-10-25 00:05:01 -0500

    feat: Implement openAITest function

--- src/libs/openai.ts
+++ src/libs/openai.ts
@@ -1,7 +1,7 @@
 import * as vscode from 'vscode';
 import OpenAI from "openai";
 import { ChatCompletion } from 'openai/resources';
-import { getAPIKey } from './sourceControl';
+import { getAPIKey } from './helpers';
 
 
 // do not hardcode api key
@@ -12,25 +12,11 @@ export async function openAITest(context: vscode.ExtensionContext, diff: String)
   const apiKey = await getAPIKey(context);
   const openai = new OpenAI({ apiKey: apiKey });
 
-  
-  const outputChannel = vscode.window.createOutputChannel('OpenAI Output Channel');
-  let terminal;
-  if (vscode.window.activeTerminal) {
-    terminal = vscode.window.activeTerminal;
-  }
-  else {
-    terminal = vscode.window.createTerminal('OpenAI');
-  }
-
   const completion = await openai.chat.completions.create({
-    messages: [{ role: "system", content: `write a git commit message for this diff thats between 50-75 charecters long with the format "<category>: <message>" where categor can be either "feat" for a new feature, "ref" for refactoring, "fix" for fixing a bug : ${diff}` }],
+    messages: [{ role: "assistant", content: `write a git commit message for this diff thats between 50-100 charecters long with the format "<category>: <message>" where categor can be either "feat" for a new feature, "ref" for refactoring, "fix" for fixing a bug : ${diff}` }],
     model: "gpt-3.5-turbo",
   });
 
   
-  // terminal.sendText(`${JSON.stringify(completion.choices[0])}`, false);
-  // outputChannel.appendLine(`${JSON.stringify(completion.choices[0])}`);
-  // outputChannel.show();
-
   return completion;
 }
